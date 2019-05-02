b7e078d0a6946fee1f02950b8dbc1b643b93abf4
'Chetan <'parmar.chetan91@gmail.com'>
2019-05-02 14:21:31 +0200

    login , update , delete added

--- demo-6/src/main/java/com/example/demo/Controller.java
+++ demo-6/src/main/java/com/example/demo/Controller.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
-import java.io.Console;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -26,18 +31,43 @@ public class Controller {
 		this.personRepository = personRepository;
 	}
 
+    @RequestMapping("/login")
+    public boolean login(@RequestBody User user) {
+        return user.getName().equals("name") && user.getEmail().equals("email");
+    }
     
 	@GetMapping("/users")
     public List<User> getUsers() {
 		return (List<User>) personRepository.findAll();
 		
+		}
+	
+	@GetMapping(value="/users/{name}")
+	 public  User getUserByName(@PathVariable("name") String name){
 		
-       
-    }
- 
+		  User user = personRepository.findByName(name);
+
+		    if(user == null){
+		     System.out.println("noone");
+		    }
+
+		    //show the student as json object
+		    return user;
+	 }
+	 @PutMapping("/users/{name}")
+	    public User updateUser(@RequestBody User user) {
+	        personRepository.save(user);
+	        return user;
+	    }
+	 
+	 @DeleteMapping("/users/{name}")
+	     void DeleteUser(@RequestBody User user, @PathVariable ("name") String name) {
+	         user = personRepository.findByName(name);
+	        personRepository.delete(user);
+	    }
     @PostMapping("/users")
     void addUser(@RequestBody User user) {
         personRepository.save(user);
     }
-    
+ 
 }
--- demo-6/src/main/java/com/example/demo/PersonRepository.java
+++ demo-6/src/main/java/com/example/demo/PersonRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends CrudRepository<User, Long>{
-	
+	User findByName(String name);
+
 }
